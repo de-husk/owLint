a05b0391c952e2f1ddc97c0ca362bdd3b57ebc59
samangan <harry.hull1@gmail.com>
2014-07-01 16:54:38 -0500

    Implemented initial args parsing

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,8 +1,39 @@
+/*
+ Usage:
+ *  This tool takes an optional argument, the absolute or relative path to the folder containing
+    the owl files in question.
+*/
 
 
 
 object owLintStarter {
-  def main (args: Array[String]) = println("todo")
+  def main (args: Array[String]) = {
+    
+    if (args.contains("-h")) {
+      println(Console.UNDERLINED + "owLint: OWL file linting tool " + Console.RESET)
+      println(Console.CYAN + "This tool takes an optional argument, the absolute or relative path to the folder containing the owl files in question." +  Console.RESET)
+      println("Examples:" + 
+        Console.GREEN + "\n   $ owlint                             " + Console.YELLOW + "| Use current directory" + 
+        Console.GREEN + "\n   $ owlint owlFileFolder               " + Console.YELLOW + "| Use ./owlFileFolder as the directory" + 
+        Console.GREEN + "\n   $ owlint /home/username/somedir      " + Console.YELLOW + "| Use absolute path /home/username/somedir as the directory" + 
+        Console.RESET)
+      sys.exit(0)
+    }
+
+
+    if (args.length == 0) {
+      // Use current dir as currentDirectory
+    } else if (args.length == 1) {
+      // Use inputted dir as currentDirectory
+    } else {
+      // Return an error and exit because this tool only takes one argument
+      Console.err.println(Console.RED + "Error: owLint only takes one argument!\nUse -h for help information." + Console.RESET)
+      sys.exit(1)
+    }
+
+
+
+  }
 }
 
 
