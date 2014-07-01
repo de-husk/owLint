a0b8a0c8901ebabda3f9d19ff7ac0fd9a11a5e21
samangan <harry.hull1@gmail.com>
2014-07-01 16:14:25 -0500

    Added test owl file and owl api dependencies

--- build.sbt
+++ build.sbt
@@ -4,3 +4,7 @@ name := "owLint"
 version := "0.0"
 
 scalaVersion := "2.10.2"
+
+libraryDependencies ++= Seq(
+  "net.sourceforge.owlapi" % "owlapi-distribution" % "3.4.5"
+)     
--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,4 +1,18 @@
 
-object owLint {
+
+
+object owLintStarter {
   def main (args: Array[String]) = println("todo")
 }
+
+
+
+/* TODO : 
+ *  This file is the entry point of the tool.
+
+ *  This file will:
+ 1. Grab .owLint file in currentDirectory and override default configurations if the file exists.
+ 2. For each OWL file in currentDirectory:
+     *  parse the OWL file with the OWL API 
+     *  pass the owl api file object (TODO: what is this specifically?) and the config object into the constructor and get the returned owLintResponse object that has true/false for if the lint was successful and then an array of errors if it wasn't
+*/
