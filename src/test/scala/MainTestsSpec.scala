149a7b5af6e8c53e5c00199747e12853c34ef0cc
Samangan <harry.hull1@gmail.com>
2014-07-08 17:15:14 -0500

    Added isValidDirectoryPath tests

--- src/test/scala/MainTestsSpec.scala
+++ src/test/scala/MainTestsSpec.scala
@@ -5,7 +5,6 @@ class MainTestsSpec extends FlatSpec with Matchers {
   "getCurrentDirectoy" should "return current directory if none is specified" in {
     val fakeArgs = Array[String]()
     val currentDirectory = owLintStarter.getCurrentDirectory(fakeArgs).get
-    
     assert(currentDirectory == System.getProperty("user.dir"))
   } 
 
@@ -13,15 +12,31 @@ class MainTestsSpec extends FlatSpec with Matchers {
     val argument = "test"
     val fakeArgs = Array(argument)
     val currentDirectory = owLintStarter.getCurrentDirectory(fakeArgs).get
-
     assert(currentDirectory == argument)
   }
 
   it should "return None if inputted more than one argument" in {
     val fakeArgs = Array("wut", "I", "doing?")
     val currentDirectory = owLintStarter.getCurrentDirectory(fakeArgs)
-
     assert(currentDirectory == None)
   }
 
+
+  "isValidDirectoryPath" should "return true for a valid directory" in {
+    val validDirectoryPath = "./test"
+    val isValidDirectoryPath = owLintStarter.isValidDirectoryPath(validDirectoryPath)
+    assert(isValidDirectoryPath == true)
+  }
+
+  it should "return false for a valid file path to a non-folder" in {
+    val filePath = "./README.md"
+    val isValidDirectoryPath = owLintStarter.isValidDirectoryPath(filePath)
+    assert(isValidDirectoryPath == false)
+  }
+
+  it should "return false for an incorrect file path" in {
+    val invalidPath = "./assdasdadasdasdasd"
+    val isValidDirectoryPath = owLintStarter.isValidDirectoryPath(invalidPath)
+    assert(isValidDirectoryPath == false)
+  }
 }
